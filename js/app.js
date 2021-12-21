cf450230b59e06a2fa254a5be627634692355573
Maxi Mongelos <maximongelos@gmail.com>
2021-12-21 02:20:03 -0300

    Creando carpeta assets y agregando icono

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
+"use strict";
+
 //QuerySelector sirve para capturar cualquier elemento del dom
-const color = document.querySelector("#color")
-const salida = document.querySelector("#salida")
+const color = document.querySelector("#color");
+const salida = document.querySelector("#salida"); //addEventListener sirve para capturar eventos
 
-//addEventListener sirve para capturar eventos
-color.addEventListener("input", (e) =>{
-    const value = color.value; 
-    salida.innerHTML = value
-    document.body.style.background = value
-    document.body.style.color = "#FFFFFF"
-    console.log(value)
-})
-
+color.addEventListener("input", () => {
+  const value = color.value;
+  salida.innerHTML = value;
+  document.body.style.background = value;
+  document.body.style.color = "#FFFFFF";
+  console.log(value);
+});
